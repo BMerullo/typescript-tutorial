52954b6cb10a8bdb692018a3f1dd5bdbf62dcc49
Bob Merullo <bmerullo85@gmail.com>
2024-09-24 14:10:49 -0600

    Pokedex styling

--- tutorial-1/client/app/components/PokedexList.tsx
+++ tutorial-1/client/app/components/PokedexList.tsx
@@ -8,7 +8,7 @@ interface PokedexListProps {
 const PokedexList: React.FC<PokedexListProps> = ({ pokedexList }) => {
   return (
     <>
-      <section className="p-10  md:p-20 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
+      <section className="p-9  md:p-20 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
         {pokedexList.map((pokemon, index) => (
           <button className="m-2 bg-blue-800 hover:bg-yellow-500  md:text-xl p-8  rounded-lg">
             <h3 key={index}>{pokemon.name}</h3>
